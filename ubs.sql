9b351de3e7f94d7e7f50d479a3e0f7a1a804ae74
Romulo Gimenes <romulo.sales@etec.sp.gov.br>
2025-05-19 20:33:41 -0300

    feat: add constraints in "paciente" and insert values (DML)

--- ubs.sql
+++ ubs.sql
@@ -2,11 +2,36 @@ CREATE DATABASE ubs;
 
 USE ubs;
 
+#DDL 
 CREATE TABLE paciente(
   cod_paciente int not null auto_increment,
   nome varchar(15) not null,
   sobrenome varchar(45) not null,
-  cpf bigint(11) not null unique,
+  cpf bigint(11) not null,
   rg varchar(8) not null,
-  cns bigint(15) not null
-)
+  cns bigint(15) not null,
+  comorbidade varchar(20) not null, 
+  deficiencia varchar(20) not null,
+  idade tinyint not null,
+  nascimento date not null,
+  tipo_sanguineo varchar(2) not null,
+  CONSTRAINT PK_codPaciente PRIMARY KEY(cod_paciente),
+  CONSTRAINT UN_cpf UNIQUE(cpf)
+);
+
+#DML
+INSERT INTO paciente
+VALUES(
+  default, 
+  "Beatriz", 
+  "Almeida", 
+  35789832145689, 
+  "123456789", 
+  56789412358, 
+  "Arritmia cardiaca, diabetes.", 
+  "Deficiencia Fisica",
+  22,
+  2003/02/13,
+  "O+"  
+  
+); 
