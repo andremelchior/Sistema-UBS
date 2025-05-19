5032c395372a88af5734583e9ecab702598e619f
André Melchior <andre.melchior@outlook.com>
2025-05-19 18:58:14 -0300

    feat: add login form component with bootstrap

---
+++ form.php
@@ -0,0 +1,15 @@
+    <div style="display: flex; width: 70%; margin: auto; justify-content: center; align-items: center;">
+
+        <form action="dashboard.php" method="POST" style="margin-top: 200px;">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input w6 class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+
+          <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
