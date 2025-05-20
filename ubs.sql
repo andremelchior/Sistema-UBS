78637c3040b54258515f9e5f872e913e02083011
Daniel Melo <79672542+DanielMelo0@users.noreply.github.com>
2025-05-19 21:53:14 -0300

    feat: add email attribute in table structure.

--- ubs.sql
+++ ubs.sql
@@ -7,9 +7,10 @@ CREATE TABLE paciente(
   cod_paciente int not null auto_increment,
   nome varchar(15) not null,
   sobrenome varchar(45) not null,
+  email varchar(45) not null,
   cpf bigint(11) not null,
-  rg varchar(8) not null,
-  cns bigint(15) not null,
+  rg varchar(9) not null, 
+  cns bigint(15) not null, 
   comorbidade varchar(40) not null, 
   deficiencia varchar(30) not null,
   idade tinyint not null,
@@ -17,7 +18,7 @@ CREATE TABLE paciente(
   tipo_sanguineo varchar(2) not null,
   CONSTRAINT PK_codPaciente PRIMARY KEY(cod_paciente),
   CONSTRAINT UN_cpf UNIQUE(cpf),
-  CONSTRAINT UN_cns UNIQUE(cns)
+  CONSTRAINT UN_cns UNIQUE(cns) 
 );
 
 #DML
@@ -32,9 +33,11 @@ VALUES(
   "Arritmia cardiaca, diabetes.", 
   "Deficiencia Fisica",
   22,
-  2003/02/13,
+  2003-02-13,
   "O+"  
   
 ); 
 
-SELECT * FROM paciente;
+SELECT * from paciente;
+
+DROP TABLE Paciente;
