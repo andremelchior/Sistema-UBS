fcaeef1b995a4d6c94dacc92dc4a027db4cb9d68
Daniel Melo <79672542+DanielMelo0@users.noreply.github.com>
2025-05-19 22:06:22 -0300

    feat: add PDO localhost connection file.

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'ubs';
+$user = 'root';
+$password = 'root';
+try {
+// DSN Data Source Name
+// Definir Tipo do Banco, Host e DB Name
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+
+// Cria a Instancia PDO 
+$pdo = new PDO($dsn,$user,$password);
+echo "Conexão com o banco de dados foi bem sucedida! <hr>";
+} catch(PDOException $e){
+  // Captura Erro de Conexão e encerra o Script com Mensagem
+
+  die("Erro na Conexão" . $e->getMessage());
+
+}
+?>
